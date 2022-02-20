bc8ec62f2a012096c415b183816b53e88bebbbc8
hs23876156 <hs23876156@gmail.com>
2022-02-21 02:06:27 +0800

    add 7000 port for ctfd test and create test.py

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
+from flask import Flask
+
 
 if __name__ == '__main__':
+
+    app = Flask(__name__)
+    @app.route("/")
+    def hello():
+        return "Hello, World!"
+    
+    app.run(host="0.0.0.0", port=7000)
+
     print("GG inin der!!!")
+
